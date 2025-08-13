730cfbc5ddba4dc11d9064d03f3e220776483162
pranav-8660 <pranavcse2003@gmail.com>
2025-08-13 10:43:08 +0530

    initial stater application for product_inventory_service

---
+++ priduct-inventory-service/src/main/java/com/pranavv51/inventorymanagementservice/priduct_inventory_service/PriductInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pranavv51.inventorymanagementservice.priduct_inventory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriductInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriductInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ priduct-inventory-service/src/test/java/com/pranavv51/inventorymanagementservice/priduct_inventory_service/PriductInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranavv51.inventorymanagementservice.priduct_inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriductInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

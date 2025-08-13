b79da76089ad95d1db099245f25166ebd83218a9
pranav-8660 <pranavcse2003@gmail.com>
2025-08-13 11:35:00 +0530

    setting up the repositories

---
+++ product-inventory-service/src/main/java/com/pranavv51/inventorymanagementservice/priduct_inventory_service/repository/InventoryRepo.java
@@ -0,0 +1,7 @@
+package com.pranavv51.inventorymanagementservice.priduct_inventory_service.repository;
+
+import com.pranavv51.inventorymanagementservice.priduct_inventory_service.entity.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepo extends JpaRepository<Inventory,Long> {
+}
---
+++ product-inventory-service/src/main/java/com/pranavv51/inventorymanagementservice/priduct_inventory_service/repository/ProductRepo.java
@@ -0,0 +1,7 @@
+package com.pranavv51.inventorymanagementservice.priduct_inventory_service.repository;
+
+import com.pranavv51.inventorymanagementservice.priduct_inventory_service.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepo extends JpaRepository<Product,Long> {
+}

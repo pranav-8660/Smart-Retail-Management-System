f3864e66a87274fe8dd4e88a88a249ba056a8c1b
pranav-8660 <pranavcse2003@gmail.com>
2025-08-13 14:54:35 +0530

    file name change

--- product-inventory-service/src/main/java/com/pranavv51/inventorymanagementservice/priduct_inventory_service/ProductInventoryServiceApplication.java
+++ product-inventory-service/src/main/java/com/pranavv51/inventorymanagementservice/priduct_inventory_service/ProductInventoryServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PriductInventoryServiceApplication {
+public class ProductInventoryServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PriductInventoryServiceApplication.class, args);
+		SpringApplication.run(ProductInventoryServiceApplication.class, args);
 	}
 
 }

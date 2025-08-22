e516ff9ad0c1a81ef5949aa3be39da7c391763e8
pranav-8660 <pranavcse2003@gmail.com>
2025-08-22 05:12:33 +0000

    added a starter for user-service

---
+++ secure-user-management-service/src/main/java/com/pranavv51/userservice/secure_user_management_service/SecureUserManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pranavv51.userservice.secure_user_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureUserManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureUserManagementServiceApplication.class, args);
+	}
+
+}
---
+++ secure-user-management-service/src/test/java/com/pranavv51/userservice/secure_user_management_service/SecureUserManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranavv51.userservice.secure_user_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureUserManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

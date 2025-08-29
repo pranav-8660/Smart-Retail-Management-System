7e1ba3f703e36661e066fd7fa3d75bdb26a0533e
pranav-8660 <pranavcse2003@gmail.com>
2025-08-29 04:03:45 +0000

    added the securityconfig class, a configuration class with filterchain and a bcrypt password encoder

---
+++ secure-user-management-service/src/main/java/com/pranavv51/userservice/secure_user_management_service/security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.pranavv51.userservice.secure_user_management_service.security;
+
+
+import com.pranavv51.userservice.secure_user_management_service.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public SecurityConfig(JwtFilter jwtFilter, CustomUserDetailsService customUserDetailsService) {
+        this.jwtFilter = jwtFilter;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain returnSecurityFilterChain(HttpSecurity httpSecurity) throws Exception{
+
+        var securityfilterchain = httpSecurity.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(
+
+                authObject -> authObject.requestMatchers("/auth/*").permitAll()
+                        .anyRequest().authenticated()
+
+        ).userDetailsService(customUserDetailsService).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class).build();
+
+        return securityfilterchain;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
